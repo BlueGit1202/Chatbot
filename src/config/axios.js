f06804f82f263129cb72b0ac4e084d7643bcd60a
MichelSmith <superdeceamyun@gmail.com>
2025-04-07 12:39:40 +0100

    aa

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,7 +3,7 @@ const openRouterApi = axios.create({
   baseURL: 'https://openrouter.ai/api/v1',
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer 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`,
+    'Authorization': `Bearer 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`,
     'HTTP-Referer': 'www.cvccleaning.co.uk',
     'X-Title':'Alex'
   }
