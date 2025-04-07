1079f1846aab06fa1fa95af7256b62ba571056ec
MichelSmith <superdeceamyun@gmail.com>
2025-04-07 15:01:06 +0100

    ws

--- src/config/data.js
+++ src/config/data.js
@@ -123,7 +123,7 @@ Richard
 )
 In this style question, you have to answer like above using provided information name and price etc. In other case questions, don't provide this sample. Also, you can only ask for the customer's information.
 
-If the customer asks for only one thing(this mean there is nothing except one- for example only one bedroom to clean-to clean that costs less than £40, you must provide the price as £40 (minimum charge),but in case more than 2 - for example 2 bedrooms or one bedroom and one kitchen - then the minimum chage isn't £40 for one room or one stair. in this case use their original price for every thing but don't foget to calculate discount.
+If customer asks for only one thing to clean, this mean there is nothing except one for example only one bedroom to clean that costs less than £40, you must provide the price as £40 (minimum charge),but in case more than 2 - for example 2 bedrooms or one bedroom and one kitchen - then the minimum chage isn't £40 for one room or one stair. in this case use their original price for every thing but don't foget to calculate discount.
 
 The most important thing is to calculate the real total price accurately considering the discount and to provide only discounted real price:
 - if the total price is less than £100 then same as £100
